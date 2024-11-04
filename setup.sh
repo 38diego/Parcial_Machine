b22ee6ee8d96a776904e0558b2dd7eba7990f429
38diego <119978015+38diego@users.noreply.github.com>
2024-11-04 03:34:23 +0000

    renamed:    package.txt -> packages.txt 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-mkdir -p ~/.streamlit/
-
-echo "Actualizando lista de paquetes..."
-apt-get update -y
-
-echo "Instalando R..."
-apt-get install -y r-base
-
-echo "Verificando instalación de Rscript..."
-if [ -f /usr/bin/Rscript ]; then
-    echo "Rscript encontrado en /usr/bin/Rscript"
-else
-    echo "Rscript no se encontró en /usr/bin/"
-    exit 1
-fi
-
-" > ~/.streamlit/config.toml
+sudo apt update
+sudo apt install -y r-base
